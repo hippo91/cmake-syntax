e7689707f6851fdea629b556a80cd505f13a0feb
Guillaume Peillex <guillaume.peillex@gmail.com>
2024-12-21 15:59:29 +0100

    Enhance error messages

--- DumpAst.hs
+++ DumpAst.hs
@@ -1,13 +1,14 @@
 import System.Environment
+import Data.List (intercalate)
 
 usage:: String
 usage = "Usage: DumpAst <filename>"
 
 parseArgs:: [String] -> (String, String)
 parseArgs (_head:_tail) = (filename, error_message) where
-  error_message = if _tail /= [] then usage else ""
+  error_message = if not $ null _tail then "Unknown argument(s): " ++ intercalate "," _tail ++ ". " ++ usage else ""
   filename = if null _tail then _head else ""
-parseArgs _ = ("", usage)
+parseArgs _ = ("Needs one argument! ", usage)
 
 main :: IO ()
 main = do
