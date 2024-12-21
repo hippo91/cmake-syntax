f59845a6bb173ca6ff12de7eab3cab28ed7cc529
Guillaume Peillex <guillaume.peillex@gmail.com>
2024-12-21 18:55:23 +0100

    Add parseFile function and use parseFromFile

--- DumpAst.hs
+++ DumpAst.hs
@@ -1,5 +1,8 @@
 import System.Environment
 import Data.List (intercalate)
+import Language.CMake.Parser (fileParser)
+import Text.Trifecta.Parser (parseFromFile)
+import Data.Foldable (forM_)
 
 usage:: String
 usage = "Usage: DumpAst <filename>"
@@ -8,7 +11,15 @@ parseArgs:: [String] -> (String, String)
 parseArgs (_head:_tail) = (filename, error_message) where
   error_message = if not $ null _tail then "Unknown argument(s): " ++ intercalate "," _tail ++ ". " ++ usage else ""
   filename = if null _tail then _head else ""
-parseArgs _ = ("Needs one argument! ", usage)
+parseArgs _ = ("", "Needs one argument! " ++ usage)
+
+parseFile:: String -> IO ()
+parseFile filename = do
+  putStrLn ("Parsing file: " ++ filename ++ "...")
+  do
+    result <- parseFromFile fileParser filename
+    forM_ result print
+  putStrLn "Done."
 
 main :: IO ()
 main = do
@@ -17,5 +28,4 @@ main = do
   if error_message /= "" then
     putStrLn error_message
   else do
-    putStrLn ("Parsing file: " ++ filename ++ "...")
-    putStrLn "Done."
+    parseFile filename
